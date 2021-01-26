0a30e0b8952b2f5a29aee02f6cc03a4f5079399c
Ivan Woo <iwoo94@gmail.com>
2021-01-26 00:13:02 -0800

    rendered responses for each question, implemented button functionalities to flip through pages for both reviews and questions

--- client/src/components/questions/QuestionsAllContent.jsx
+++ client/src/components/questions/QuestionsAllContent.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import axios from 'axios';
 import QuestionsList from './QuestionsList.jsx';
+import QuestionsPageBar from './QuestionsPageBar.jsx';
 
 class QuestionsAllContent extends React.Component {
   constructor() {
     super();
     this.state = {
       questions: [],
-      responses: []
+      start: 0,
+      end: 10
     };
     this.getQuestions = this.getQuestions.bind(this);
     this.getResponses = this.getResponses.bind(this);
+    this.nextPage = this.nextPage.bind(this);
+    this.previousPage = this.previousPage.bind(this);
   }
 
   componentDidMount() {
@@ -35,13 +39,35 @@ class QuestionsAllContent extends React.Component {
       .catch()
   }
 
+  nextPage() {
+    this.setState((state) => ({
+      start: state.start + 10,
+      end: state.end + 10
+    }), () => {
+      console.log(this.state)
+    })
+  }
+
+  previousPage() {
+    this.setState((state) => ({
+      start: state.start - 10,
+      end: state.end - 10
+    }), () => {
+      console.log(this.state)
+    })
+  }
+
   render() {
+    let { questions, start, end} = this.state;
+    let displayQuestions = questions.slice(start, end);
+
     return (
       <div className="questions-all-content-container">
         <div className="ask-question-button-container">
           <button className="ask-question-button">ASK A PRODUCT QUESTION</button>
         </div>
-        <QuestionsList questions={this.state.questions}/>
+        <QuestionsList questions={displayQuestions}/>
+        <QuestionsPageBar questions={this.state.questions} nextPage={this.nextPage} previousPage={this.previousPage}/>
       </div>
     )
   }
